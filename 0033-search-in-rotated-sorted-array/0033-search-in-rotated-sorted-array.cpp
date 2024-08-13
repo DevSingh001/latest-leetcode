0e04bc2c34b623dc6e0bec998d67d22d72834443
DevSingh001 <101054577+DevSingh001@users.noreply.github.com>
2024-08-12 22:46:08 -0700

    Time: 6 ms (16.93%), Space: 13.9 MB (21.37%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,53 +1,56 @@
 class Solution {
 public:
-    int pivot(vector<int>& nums){
-        int n=nums.size();
+    int pivot(vector<int>& v){
         int s=0;
-        int e=n-1;
+        int e=v.size()-1;
         while(s<=e){
             int mid=s+(e-s)/2;
             if(s==e){
                 return s;
             }
-            else if(mid>0 && nums[mid]<nums[mid-1]){
+            else if(mid+1<v.size() && v[mid]>v[mid+1]){
+                return mid;
+            }
+            else if(mid-1>0 && v[mid]<v[mid-1]){
                 return mid-1;
             }
-            else if(mid+1<n && nums[mid]>nums[mid+1]){
-                return mid;
-            }
-            else if(nums[mid]>=nums[s]){
-                s=mid+1;
+            else if(v[s]>v[mid]){
+                e=mid-1;
             }
             else{
-                e=mid-1;
+                s=mid+1;
             }
         }
         return -1;
     }
-    int BinarySearch(vector<int>& nums,int s,int e,int target){
-   
+    int binary(vector<int>& vec,int s,int e, int target){
+    
         while(s<=e){
             int mid=s+(e-s)/2;
-            if(nums[mid]==target){
+            if(vec[mid]==target){
                 return mid;
             }
-            else if(target>nums[mid]){
-                s=mid+1;
-            }
-            else{
+            else if(vec[mid]>target){
                 e=mid-1;
             }
+            else{
+                s=mid+1;
+            }
         }
         return -1;
     }
+    
     int search(vector<int>& nums, int target) {
-        int piv=pivot(nums);
-        if(target>=nums[0] && target<=nums[piv]){
-             return BinarySearch(nums,0,piv,target);
+        int pivotInd=pivot(nums);
+        int n=nums.size();
+        int ans=-1;
+        if(target>=nums[0] && target<=nums[pivotInd]){
+            ans=binary(nums,0,pivotInd,target);
         }
-        for(int i=piv+1;i<nums.size();i++){
-           return BinarySearch(nums,piv+1,nums.size()-1,target);
+        else{
+            ans=binary(nums,pivotInd+1,n-1,target);
         }
-        return -1;
+        return ans;
     }
+    
 };
