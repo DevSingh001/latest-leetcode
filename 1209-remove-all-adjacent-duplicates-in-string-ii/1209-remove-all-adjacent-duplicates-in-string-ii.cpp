d14bda2d49fd8781181e7f062c2646c5e3eb9e34
DevSingh001 <101054577+DevSingh001@users.noreply.github.com>
2024-08-14 17:48:50 +0530

    Time: 12 ms (89.33%), Space: 12.4 MB (56.10%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
@@ -1,28 +1,45 @@
 class Solution {
 public:
-    bool checkK_1Elements(string &ans,int k,char &ch){
-        int it=ans.length()-1;
-        for(int i=0;i<k-1;i++){
-            if(ch!=ans[it]) return false;
-            else it--;
-        }
-        return true;
-    }
+    // bool checkK_1Elements(string &ans,int k,char &ch){
+    //     int it=ans.length()-1;
+    //     for(int i=0;i<k-1;i++){
+    //         if(ch!=ans[it]) return false;
+    //         else it--;
+    //     }
+    //     return true;
+    // }
     string removeDuplicates(string s, int k) {
-        string ans="";
-        for(auto ch:s){
-            if(ans.length()<k-1){
-                ans.push_back(ch);
+        // string ans="";
+        // for(auto ch:s){
+        //     if(ans.length()<k-1){
+        //         ans.push_back(ch);
+        //     }
+        //     else if(checkK_1Elements(ans,k,ch)){
+        //         for(int i=0;i<k-1;i++){
+        //             ans.pop_back();
+        //         }
+        //     }
+        //     else{
+        //         ans.push_back(ch);
+        //     }
+        // }
+        // return ans;
+        
+        int i=0;
+        int j=0;
+        vector<int>count(s.length());
+        while(j<s.length()){
+            s[i]=s[j];
+            count[i]=1;
+            if(i>0 && s[i]==s[i-1]){
+                count[i]+=count[i-1];
             }
-            else if(checkK_1Elements(ans,k,ch)){
-                for(int i=0;i<k-1;i++){
-                    ans.pop_back();
-                }
+            if(count[i]==k){
+                i-=k;
             }
-            else{
-                ans.push_back(ch);
-            }
+            i++;
+            j++;
         }
-        return ans;
+        return s.substr(0,i);
     }
 };
