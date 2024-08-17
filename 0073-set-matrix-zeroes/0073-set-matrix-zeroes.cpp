37081ff588129db5205a92701aeca9248a6bce8e
DevSingh001 <101054577+DevSingh001@users.noreply.github.com>
2024-08-17 14:47:28 +0530

    Time: 8 ms (77.01%), Space: 17.1 MB (14.36%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -23,10 +23,5 @@ public:
                 matrix[i][cols[k]]=0;
             }
         }
-        for(int i=0;i<row;i++){
-            for(int j=0;j<col;j++){
-                cout<<matrix[i][j];
-            }
-        }
     }
 };
