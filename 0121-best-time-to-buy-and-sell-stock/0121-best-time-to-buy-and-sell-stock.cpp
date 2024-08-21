6cf2cb3969695a6217bc14339c270544c269c70a
DevSingh001 <101054577+DevSingh001@users.noreply.github.com>
2024-08-21 13:39:31 +0530

    Time: 75 ms (90.28%), Space: 96.3 MB (20.24%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int ans = 0;  // Variable to store the maximum profit.
-        int minPrice = INT_MAX;  // Initialize minPrice to the maximum possible integer value.
+        int ans = 0;  
+        int minPrice = INT_MAX; 
         
-        // Traverse the array from the beginning to the end.
+        
         for (int i = 0; i < prices.size(); i++) {
-            // Update the minimum price if the current price is lower.
             if (prices[i] < minPrice) {
                 minPrice = prices[i];
             }
-            // Calculate the potential profit for the current day.
             int profit = prices[i] - minPrice;
-            // Update the maximum profit if the current profit is higher.
             if (profit > ans) {
                 ans = profit;
             }
